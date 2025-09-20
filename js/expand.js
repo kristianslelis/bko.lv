d66e2dd1628a963ec42381b3d97c223712c1f661
kristianslelis <79762450+kristianslelis@users.noreply.github.com>
2025-09-20 14:53:30 +0200

    Delete js directory

--- js/expand.js
+++
@@ -1,9 +0,0 @@
-function expandImage(src) {
-    document.getElementById("fullImage").src = src.replace("thumbnail", "fullsize"); // Optional: replace with larger image path
-    document.getElementById("imageModal").style.display = "block";
-  }
-
-  // Close the modal
-  function closeModal() {
-    document.getElementById("imageModal").style.display = "none";
-  }
